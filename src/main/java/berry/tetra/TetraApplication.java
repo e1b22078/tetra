2c8b818cb082285e0f2e8f71e2c2520fe551ac85
e1b22063 <e1b22063@oit.ac.jp>
2024-10-29 16:51:18 +0900

    初期化

---
+++ src/main/java/berry/tetra/TetraApplication.java
@@ -0,0 +1,13 @@
+package berry.tetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/berry/tetra/TetraApplicationTests.java
@@ -0,0 +1,13 @@
+package berry.tetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
