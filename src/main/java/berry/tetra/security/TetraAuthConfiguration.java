7d605f0a8d6497f870c94b9a31012a8c0b3ce89f
e1b22057 <e1b22057@oit.ac.jp>
2024-11-08 10:00:24 +0900

    security内容の変更

--- src/main/java/berry/tetra/security/TetraAuthConfiguration.java
+++ src/main/java/berry/tetra/security/TetraAuthConfiguration.java
@@ -13,13 +13,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class TetraAuthConfiguration {
-  /**
-   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
-   *
-   * @param http
-   * @return
-   * @throws Exception
-   */
+  // 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.formLogin(login -> login
@@ -28,15 +22,26 @@ public class TetraAuthConfiguration {
             .logoutUrl("/logout")
             .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
         .authorizeHttpRequests(authz -> authz
-            .requestMatchers(AntPathRequestMatcher.antMatcher("/admin/**"))
-            .authenticated()
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/admin"))
+            .authenticated() // /admin以下は認証済みであること
             .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
             .permitAll())// 上記以外は全員アクセス可能
         .csrf(csrf -> csrf
-            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))// h2-console用にCSRF対策を無効化
+            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))
         .headers(headers -> headers
             .frameOptions(frameOptions -> frameOptions
                 .sameOrigin()));
     return http.build();
+  }
+
+  // 認証処理に関する設定（誰がどのようなロールでログインできるか）
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    UserDetails admin = User.withUsername("admin")
+        .password("{bcrypt}$2y$05$0yY6OCJ67KYm7ScWMVYKJeyGQ/9Swc5eKaiq7foC6uxZ7bVnkJiRi").roles("ADMIN").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す
+    return new InMemoryUserDetailsManager(admin);
   }
 }
