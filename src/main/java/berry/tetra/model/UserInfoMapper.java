89aabb69b21e48c53803f7a2b92157c5ee8dd857
e1b22078 <e1b22078@oit.ac.jp>
2024-11-10 20:09:53 +0900

    player.htmlできた

--- src/main/java/berry/tetra/model/UserInfoMapper.java
+++ src/main/java/berry/tetra/model/UserInfoMapper.java
@@ -4,11 +4,16 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
-  @Select("SELECT id,userName from userinfo where id = #{id}")
+
+  @Select("SELECT id, userName FROM userinfo WHERE id = #{id}")
   UserInfo selectById(int id);
+
+  @Select("SELECT id, userName FROM userinfo")  // すべてのユーザー情報を取得するSQL
+  List<UserInfo> selectAllUsers();  // すべてのユーザーを返すメソッド
 
   @Insert("INSERT INTO userinfo (userName) VALUES (#{userName});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
