33cd84366e6600d0044e2fb61301b4954036c7b2
e1b22063 <e1b22063@oit.ac.jp>
2024-11-12 15:25:52 +0900

    ユーザー名の重複を解消

--- src/main/java/berry/tetra/controller/PlayerController.java
+++ src/main/java/berry/tetra/controller/PlayerController.java
@@ -13,30 +13,35 @@ import java.util.List;
 @Controller
 public class PlayerController {
 
-    @Autowired
-    UserInfoMapper userInfoMapper;
+  @Autowired
+  UserInfoMapper userInfoMapper;
 
-    // 名前入力ページへのGETリクエスト
-    @GetMapping("/name")
-    public String name() {
-        return "name.html";
+  // 名前入力ページへのGETリクエスト
+  @GetMapping("/name")
+  public String name() {
+    return "name.html";
+  }
+
+  // プレイヤー情報を表示するページ
+  @GetMapping("/player")
+  public String player(@RequestParam("playername") String playername, Model model) {
+    // プレイヤー名をデータベースに保存
+    if (userInfoMapper.selectByName(playername) == null) {
+      UserInfo userInfo = new UserInfo();
+      userInfo.setUserName(playername);
+      userInfoMapper.insertUserInfo(userInfo);
+    } else {
+      model.addAttribute("error", playername);
+      return "name.html";
     }
 
-    // プレイヤー情報を表示するページ
-    @GetMapping("/player")
-    public String player(@RequestParam("playername") String playername, Model model) {
-        // プレイヤー名をデータベースに保存
-        UserInfo userInfo = new UserInfo();
-        userInfo.setUserName(playername);
-        userInfoMapper.insertUserInfo(userInfo);
+    // 登録されている全ユーザー情報を取得
+    List<UserInfo> allUsers = userInfoMapper.selectAllUsers();
 
-        // 登録されている全ユーザー情報を取得
-        List<UserInfo> allUsers = userInfoMapper.selectAllUsers();
+    // モデルにプレイヤー名とユーザーリストを追加
+    model.addAttribute("playername", playername);
+    model.addAttribute("allUsers", allUsers);
 
-        // モデルにプレイヤー名とユーザーリストを追加
-        model.addAttribute("playername", playername);
-        model.addAttribute("allUsers", allUsers);
-
-        return "player.html";
-    }
+    return "player.html";
+  }
 }
--- src/main/java/berry/tetra/model/UserInfoMapper.java
+++ src/main/java/berry/tetra/model/UserInfoMapper.java
@@ -9,11 +9,11 @@ import java.util.List;
 @Mapper
 public interface UserInfoMapper {
 
-  @Select("SELECT id, userName FROM userinfo WHERE id = #{id}")
-  UserInfo selectById(int id);
+  @Select("SELECT id, userName FROM userinfo WHERE userName = #{userName}")
+  UserInfo selectByName(String userName);
 
-  @Select("SELECT id, userName FROM userinfo")  // すべてのユーザー情報を取得するSQL
-  List<UserInfo> selectAllUsers();  // すべてのユーザーを返すメソッド
+  @Select("SELECT id, userName FROM userinfo") // すべてのユーザー情報を取得するSQL
+  List<UserInfo> selectAllUsers(); // すべてのユーザーを返すメソッド
 
   @Insert("INSERT INTO userinfo (userName) VALUES (#{userName});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
