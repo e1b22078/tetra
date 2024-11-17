97040d244d4459b7c4d1e6e2958ffe7fda7a8cb0
e1b22078 <e1b22078@oit.ac.jp>
2024-11-17 21:21:51 +0900

    admin,DB上でユーザ情報の削除が可能

--- src/main/java/berry/tetra/controller/AdminController.java
+++ src/main/java/berry/tetra/controller/AdminController.java
@@ -1,14 +1,31 @@
 package berry.tetra.controller;
 
+import berry.tetra.model.UserInfo;
+import berry.tetra.model.UserInfoMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminController {
 
+  @Autowired
+  private UserInfoMapper userInfoMapper;
+
+  // ユーザー一覧を表示
   @GetMapping("/admin")
-  public String admin() {
-    return "admin.html";
+  public String admin(Model model) {
+    model.addAttribute("users", userInfoMapper.selectAllUsers());
+    return "admin";
   }
 
+  // ユーザー削除
+  @GetMapping("/admin/delete/{id}")
+  public String deleteUser(@PathVariable("id") int id) {
+    userInfoMapper.deleteUserById(id);
+    return "redirect:/admin";  // 削除後にユーザー一覧ページにリダイレクト
+  }
 }
--- src/main/java/berry/tetra/model/UserInfoMapper.java
+++ src/main/java/berry/tetra/model/UserInfoMapper.java
@@ -1,6 +1,8 @@
 package berry.tetra.model;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
@@ -21,4 +23,16 @@ public interface UserInfoMapper {
   @Insert("INSERT INTO userinfo (userName, psswd) VALUES (#{userName}, #{psswd});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertUserInfo(UserInfo userInfo);
+
+  @Select("SELECT id, userName, psswd FROM userinfo WHERE id = #{id}")
+  UserInfo selectById(int id);
+
+  @Delete("DELETE FROM userinfo WHERE id = #{id}")
+  void deleteUserById(int id);
+
+  @Update("UPDATE userinfo SET userName = #{userName}, psswd = #{psswd} WHERE id = #{id}")
+  void updateUser(UserInfo userInfo);
+
+  @Select("SELECT * FROM user_info WHERE user_name = #{userName}")
+  UserInfo selectByUserName(String userName);
 }
