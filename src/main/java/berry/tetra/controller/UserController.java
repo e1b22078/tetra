c8e7318a076362f3b85e2574be2b2d340c903c08
e1b22078 <e1b22078@oit.ac.jp>
2024-11-26 15:45:37 +0900

    UserController追加

---
+++ src/main/java/berry/tetra/controller/UserController.java
@@ -0,0 +1,25 @@
+package berry.tetra.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import berry.tetra.model.UserInfo;
+import berry.tetra.model.UserInfoMapper;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+@Autowired
+UserInfoMapper userInfoMapper;
+
+@GetMapping
+public List<UserInfo> getUserInfo(){
+  return userInfoMapper.selectAllUsers();
+}
+
+}
